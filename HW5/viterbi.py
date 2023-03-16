76d3cf167e72a04f99340f53cc899db8996fa6a3
nebHailemariam <nebhailemariam@gmail.com>
2023-03-16 19:36:32 +0200

    feat: save progress

--- HW5/viterbi.py
+++ HW5/viterbi.py
@@ -15,6 +15,11 @@ OOV_WORD = "OOV"  # check that the HMM file uses this same string
 INIT_STATE = "init"  # check that the HMM file uses this same string
 FINAL_STATE = "final"  # check that the HMM file uses this same string
 
+"""
+    The following code was adopted from
+    https://github.com/melanietosik/viterbi-pos-tagger/blob/master/scripts/viterbi.py
+"""
+
 
 class Viterbi:
     def __init__(self):
@@ -63,6 +68,7 @@ class Viterbi:
         self.POSStates = self.states.copy()
         self.POSStates.remove(INIT_STATE)
         self.POSStates.remove(FINAL_STATE)
+        self.POSStates = list(self.POSStates)
 
     # run Viterbi algorithm and write the output to the output file
     def runViterbi(self):
@@ -79,78 +85,91 @@ class Viterbi:
     # =======================TO-DO=======================
     def viterbiLine(self, line):
         words = line.split()
-
-        # Initialize DP matrix for Viterbi here (we suggest using a dictionary)
-        # You will require a path probability matrix and a backpointer matrix
-
         for i, word in enumerate(words):
             # replace unseen words as oov
             if word not in self.vocab:
                 words[i] = "OOV"
 
-        O = self.vocab  # Observation space
-        S = list(self.POSStates)  # State space
-        Y = words  # Sequence of observations
-        A = self.transition  # Transition matrix
-        B = self.emission  # Emission matrix
+        # Initialize
+        viterbi, back_pointer = self.initialize(words)
 
-        N = len(O)
-        K = len(S)
-        lookup = {}
+        # Iterate
+        self.forward(words, viterbi, back_pointer)
 
-        for i, word in enumerate(O):
-            lookup[word] = i
-        T = len(Y)
-        T1 = [[0] * T for i in range(K)]
-        T2 = [[None] * T for i in range(K)]
+        # Obtain optimal sequence
+        return self.optimal_sequence(words, viterbi, back_pointer)
 
-        # Predicted tags
-        X = [None] * T
-        """
-        Initialize start probabilities
-        """
-        for i in range(K):
-            T1[i][0] = A["init"][S[i]] + B[S[i]][Y[0]]
-            T2[i][0] = 0
+    def initialize(self, words) -> list:
+        viterbi = []
+        back_pointer = []
 
-        """
-        Forward step
-        """
-        for i in range(1, T):
-            for j in range(K):
-                best_prob = float("-inf")
+        # Create a path probability matrix and viterbi matrix
+        for i in range(len(self.POSStates)):
+            viterbi.append([0] * len(words))
+            back_pointer.append([None] * len(words))
+
+        # Initialize the starting probabilities
+        for state_index in range(len(self.POSStates)):
+            viterbi[state_index][0] = (
+                self.transition["init"][self.POSStates[state_index]]
+                + self.emission[self.POSStates[state_index]][words[0]]
+            )
+            back_pointer[state_index][0] = 0
+
+        return viterbi, back_pointer
+
+    def forward(self, words, viterbi, back_pointer) -> list:
+        for word_index in range(1, len(words)):
+            for state_index in range(len(self.POSStates)):
+                best_prob = -float("inf")
                 best_path = None
 
-                for k in range(K):
-                    prob = T1[k][i - 1] + A[S[k]][S[j]] + B[S[j]][Y[i]]
+                for prev_state_index in range(len(self.POSStates)):
+                    prob = (
+                        viterbi[prev_state_index][word_index - 1]
+                        + self.transition[self.POSStates[prev_state_index]][
+                            self.POSStates[state_index]
+                        ]
+                        + self.emission[self.POSStates[state_index]][words[word_index]]
+                    )
 
                     if prob > best_prob:
                         best_prob = prob
-                        best_path = k
+                        best_path = prev_state_index
 
-                T1[j][i] = best_prob
-                T2[j][i] = best_path
+                viterbi[state_index][word_index] = best_prob
+                back_pointer[state_index][word_index] = best_path
+        return viterbi, back_pointer
 
-        """
-        Backward step
-        """
+    def optimal_sequence(self, words, viterbi, back_pointer) -> list:
         try:
-            z = [None] * T
-            argmax = T1[0][T - 1]
+            # An array containing the optimal sequence of states
+            optimal_sequence = []
+            optimal_index = []
+            # Initialize the optimal sequence
+            for _ in range(len(words)):
+                optimal_sequence.append(None)
+                optimal_index.append(None)
 
-            for k in range(1, K):
-                if T1[k][T - 1] > argmax:
-                    argmax = T1[k][T - 1]
-                    z[T - 1] = k
+            # Find the optimal state for the last word
+            arg_max = viterbi[0][len(words) - 1]
+            for state_index in range(1, len(self.POSStates)):
+                if viterbi[state_index][len(words) - 1] > arg_max:
+                    arg_max = viterbi[state_index][len(words) - 1]
+                    optimal_index[len(words) - 1] = state_index
 
-            X[T - 1] = S[z[T - 1]]
-            for i in range(T - 1, 0, -1):
-                z[i - 1] = T2[z[i]][i]
-                X[i - 1] = S[z[i - 1]]
+            # Set the optimal index for the last word
+            optimal_sequence[len(words) - 1] = self.POSStates[
+                optimal_index[len(words) - 1]
+            ]
+
+            # Iteratively backtrack to find the optimal sequence
+            for i in range(len(words) - 1, 0, -1):
+                optimal_index[i - 1] = back_pointer[optimal_index[i]][i]
+                optimal_sequence[i - 1] = self.POSStates[optimal_index[i - 1]]
         except:
             return ""
-
-        return " ".join(X)
+        return " ".join(optimal_sequence)
 
 
 if __name__ == "__main__":
